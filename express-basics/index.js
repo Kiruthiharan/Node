5d8ce93551f6a533b0664902e183e340ba692ca8
kiruthiharan <kiruthiharan@gmail.com>
2020-02-23 14:46:03 +0530

    express basics

---
+++ express-basics/index.js
@@ -0,0 +1,9 @@
+const express = require('express');
+const app=express();
+
+app.get('/',function(req,res){
+    res.send('Hello World');
+});
+
+//function optional
+app.listen(3000,()=>{console.log("Listening on port 3000")});
